a91f4a8d0cb87a5a55e81abd3ba117105d454535
Jack Millar <@jack.millar400>
2020-11-14 13:31:17 +0000

    validateCredentials and displayAccount methods created

---
+++ bank/src/main/java/service/bank/Bank.java
@@ -0,0 +1,101 @@
+package service.bank;
+
+import java.util.HashMap;
+import java.util.Map;
+import service.core.Account;
+
+public class Bank {
+
+  private long bankID;
+  private String bankName;
+  private Map<Long, Account> accounts = new HashMap<Long, Account>();
+
+  public Bank() {
+  }
+
+  public Bank(long bankID, String bankName,
+      Map<Long, Account> accounts) {
+    this.bankID = bankID;
+    this.bankName = bankName;
+    this.accounts = accounts;
+  }
+
+  public long getBankID() {
+    return bankID;
+  }
+
+  public void setBankID(long bankID) {
+    this.bankID = bankID;
+  }
+
+  public String getBankName() {
+    return bankName;
+  }
+
+  public void setBankName(String bankName) {
+    this.bankName = bankName;
+  }
+
+  public Map<Long, Account> getAccounts() {
+    return accounts;
+  }
+
+  public void setAccounts(Map<Long, Account> accounts) {
+    this.accounts = accounts;
+  }
+
+  public void addAccount(Account newAccount) {
+    this.accounts.put(newAccount.getAccountId(), newAccount);
+  }
+
+  public void dropAccount(Long accountID) {
+    this.accounts.remove(accountID);
+  }
+
+  private boolean validCredentialsCheck(Long accountID, Long pin) {
+    if (this.accounts.containsKey(accountID)) {
+      return this.accounts.get(accountID).getPinNumber() == pin;
+    }
+    return false;
+  }
+
+  public void displayAccount(Long accountID, Long pin) {
+    System.out.println(
+        "|=================================================================================================================|");
+    System.out.println(
+        "|                                     |                                     |                                     |");
+    if (validCredentialsCheck(accountID, pin)) {
+      Account account = this.accounts.get(accountID);
+      System.out.println(
+          "| ID: " + String.format("%1$-32s", account.getAccountId()) +
+              "| Name: " + String.format("%1$-29s", account.getAccHolderName()) +
+              " | Balance: " + String.format("%1$-26s", account.getBalance()) + " |");
+    } else {
+      System.out.println("INVALID CREDENTIALS");
+    }
+    System.out.println(
+        "|                                     |                                     |                                     |");
+    System.out.println(
+        "|=================================================================================================================|");
+  }
+
+  public static Map<Long, Account> importAccounts() {
+    Map<Long, Account> defaultAccounts = new HashMap();
+    defaultAccounts.put(01L, new Account(01L, 1000, "testAcc1", null, 1000));
+    defaultAccounts.put(02L, new Account(02L, 2000, "testAcc2", null, 2000));
+    defaultAccounts.put(03L, new Account(03L, 3000, "testAcc3", null, 3000));
+    defaultAccounts.put(04L, new Account(04L, 4000, "testAcc4", null, 4000));
+    defaultAccounts.put(05L, new Account(05L, 5000, "testAcc5", null, 5000));
+    return defaultAccounts;
+  }
+
+  public static void main(String[] args) {
+
+    Bank newBank = new Bank(0, "testBank", importAccounts());
+    System.out.println(newBank);
+    System.out.println(newBank.bankID);
+    System.out.println(newBank.getAccounts());
+    newBank.displayAccount(01L, 1000L);
+  }
+
+}
