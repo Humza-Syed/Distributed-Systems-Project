42f8c19deb802f9dcc94b6a4ae3d9b462db39b75
Jack Millar <@jack.millar400>
2020-12-23 11:42:59 +0000

    account model created

--- bank/src/main/java/service/model/Account.java
+++ bank/src/main/java/service/model/Account.java
@@ -1,5 +1,110 @@
 package service.model;
 
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import org.hibernate.annotations.CreationTimestamp;
+
+@Entity
 public class Account {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Integer id;
+  @Column
+  private long bankId;
+  @Column
+  private long accountId;
+  @Column
+  private int pinNumber;
+  @Column
+  private String accHolderName;
+  @Column
+  private double balance;
+  @Column
+  @CreationTimestamp
+  private Date created_date;
+  @Column
+  @CreationTimestamp
+  private Date last_updated_date;
+
+  public Account() {
+  }
+
+  public Account(Integer id, long bankId, long accountId, int pinNumber,
+      String accHolderName, double balance) {
+    this.id = id;
+    this.bankId = bankId;
+    this.accountId = accountId;
+    this.pinNumber = pinNumber;
+    this.accHolderName = accHolderName;
+    this.balance = balance;
+  }
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public long getBankId() {
+    return bankId;
+  }
+
+  public void setBankId(long bankId) {
+    this.bankId = bankId;
+  }
+
+  public long getAccountId() {
+    return accountId;
+  }
+
+  public void setAccountId(long accountId) {
+    this.accountId = accountId;
+  }
+
+  public int getPinNumber() {
+    return pinNumber;
+  }
+
+  public void setPinNumber(int pinNumber) {
+    this.pinNumber = pinNumber;
+  }
+
+  public String getAccHolderName() {
+    return accHolderName;
+  }
+
+  public void setAccHolderName(String accHolderName) {
+    this.accHolderName = accHolderName;
+  }
+
+  public double getBalance() {
+    return balance;
+  }
+
+  public void setBalance(double balance) {
+    this.balance = balance;
+  }
+
+  public Date getCreated_date() {
+    return created_date;
+  }
+
+  public void setCreated_date(Date created_date) {
+    this.created_date = created_date;
+  }
+
+  public Date getLast_updated_date() {
+    return last_updated_date;
+  }
+
+  public void setLast_updated_date(Date last_updated_date) {
+    this.last_updated_date = last_updated_date;
+  }
 }
